5a159649eb0b77c19ae8ef79d7f7ee7d9464908e
Eneko123 <eneko_iglesias@euneiz.com>
2026-03-06 13:42:06 +0100

    Comienzo de la pool

--- Assets/Eneko/Scripts/Bala.cs
+++ Assets/Eneko/Scripts/Bala.cs
@@ -1,11 +1,23 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Bala : MonoBehaviour
 {
+    public GameObject bala;
+    public List<GameObject> poolBalas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < 5; i++)
+        { 
+            if (poolBalas[i] == null)
+            {
+                Instantiate(bala);
+                bala.SetActive(false);
+                poolBalas.Add(poolBalas[i]);
+            }
+        }
     }
 
     // Update is called once per frame
